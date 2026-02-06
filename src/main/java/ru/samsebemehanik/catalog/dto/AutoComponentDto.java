1f0f873be6ae554b971ee4d15528551874f81987
konstantinius96 <konstantinius96@gmail.com>
2026-02-06 23:01:01 +0300

    Update AutoComponentDto.java

--- src/main/java/ru/samsebemehanik/catalog/dto/AutoComponentDto.java
+++ src/main/java/ru/samsebemehanik/catalog/dto/AutoComponentDto.java
@@ -1,26 +1,33 @@
-package ru.samsebemehanik.catalog.dto;
-
-public class AutoComponentDto {
-
-    private Long id;
-    private String name;
-    private String description;
-
+ package ru.samsebemehanik.catalog.dto;
+ 
+ public class AutoComponentDto {
+ 
+     private Long id;
+     private String name;
+     private String description;
+    private Long version;
+ 
     public AutoComponentDto(Long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
+    public AutoComponentDto(Long id, String name, String description, Long version) {
+         this.id = id;
+         this.name = name;
+         this.description = description;
+        this.version = version;
+     }
+ 
+     public Long getId() {
+         return id;
+     }
+ 
+     public String getName() {
+         return name;
+     }
+ 
+     public String getDescription() {
+         return description;
+     }
 
-    public Long getId() {
-        return id;
+    public Long getVersion() {
+        return version;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-}
+ }
