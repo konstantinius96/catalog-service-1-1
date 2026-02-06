678cb34f27f7018589edf49b594237a92384a3b2
konstantinius96 <konstantinius96@gmail.com>
2026-02-06 23:10:33 +0300

    Update AutoComponentService.java

--- src/main/java/ru/samsebemehanik/catalog/service/AutoComponentService.java
+++ src/main/java/ru/samsebemehanik/catalog/service/AutoComponentService.java
@@ -1,11 +1,15 @@
 package ru.samsebemehanik.catalog.service;
+ 
+import ru.samsebemehanik.catalog.dto.AutoComponentDto;
 
-import java.util.List;
+ import java.util.List;
 import java.util.Map;
-
-public interface AutoComponentService {
-
+ 
+ public interface AutoComponentService {
+ 
     List<Map<String, Object>> getAll();
-
+    List<AutoComponentDto> getAll();
+ 
     Map<String, Object> getById(Long id);
-}
+    AutoComponentDto getById(Long id);
+ }
