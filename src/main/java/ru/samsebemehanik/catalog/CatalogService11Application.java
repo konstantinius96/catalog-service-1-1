a304212957771b8c573aa7ea2ac2676af20bba1d
konstantinius96 <konstantinius96@gmail.com>
2026-02-10 23:32:48 +0300

    commit

--- src/main/java/ru/samsebemehanik/catalog/CatalogService11Application.java
+++ src/main/java/ru/samsebemehanik/catalog/CatalogService11Application.java
@@ -2,8 +2,10 @@ package ru.samsebemehanik.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CatalogService11Application {
 
     public static void main(String[] args) {
